cc8eff2275ee74256a75256da46b968d39133ee8
Jared Fleming <jaredmf10@gmail.com>
2020-03-12 17:23:09 -0400

    jared

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
+const header = document.querySelector("#header");
 console.log("Here's your header:", header)
 
+console.log(document.getElementById("header"))
 
 /***** Deliverable 2 *****/
-
+header.style.color = "red";
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+console.log("PLAYERS array looks like this:", PLAYERS)
+players = document.querySelector('.player-container')
 
+PLAYERS.forEach(function(player){
+    players.innerHTML += `
+    <div class="player" data-number="${player.number}">
+      <h3>
+        ${player.name} (<em>${player.nickname}</em>)
+      </h3>
+     <img src="${player.photo}" alt="${player.name}">
+    </div>
+    `
+})
+
+console.log("PLAYERS array looks like this:", PLAYERS)
 
 /***** Deliverable 4 *****/
+
+const deletePlayer = divContainer.querySelector(`div[data-number="7"]`).remove()
